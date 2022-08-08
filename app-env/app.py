a5c416a03adf97fba8652c17453279e14ff6e378
Francesco <personal@bilottafrancesco.com>
2022-08-08 11:57:01 -0700

    Webp support

--- app-env/app.py
+++ app-env/app.py
@@ -5,6 +5,7 @@ print(glob.glob("*.png"))
 
 for file in glob.glob("../images-working-folder/*.png"):
     im = Image.open(file)
-    rgb_im = im.convert('RGB')
-    rgb_im.save(file.replace("png", "jpg"), quality=95)
+    #rgb_im = im.convert('RGB') #for jpg
+    #rgb_im.save(file.replace("png", "jpg"), quality=95) #for jpg
+    im.save(file.replace("png", "webp"), format="webp")
 
